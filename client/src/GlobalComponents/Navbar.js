b6023578e6707329e5ad822d2c4eeede32a4f216
leowrites <75044178+leowrites@users.noreply.github.com>
2022-06-01 23:44:09 -0700

    added navbar and theme

--- client/src/GlobalComponents/Navbar.js
+++ client/src/GlobalComponents/Navbar.js
@@ -1,40 +1,118 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { useState } from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import { IconButton } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle'
+import Switch from '@mui/material/Switch'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
+import MenuItem from '@mui/material/MenuItem'
+import Menu from '@mui/material/Menu'
+import Link from '@mui/material/Link';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+export default function Navbar() {
+  const [auth, setAuth] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null)
+  // if logged in, request for friend info
 
-export default function NavBar() {
-  const classes = useStyles();
+  const handleClick = (e) => {
+    // redirect to some page
+    console.log(e)
+  }
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
+    <Box sx={{ flexGrow: 1 }}>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch
+            checked={auth}
+            onChange={e => setAuth(!auth)}
+            aria-label='login switch'
+          />}
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+      <AppBar position='static'>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Link href='/'
+            variant='h5'
+            color='inherit'
+            underline='none'
+            sx={{ mx: 2 }}
+          >
+            MyCalendar
+          </Link>
+          <Button href='/calendar'
+            variant='text'
+            color='inherit'
+            sx={{
+              fontSize: 16,
+              position: 'absolute',
+              left: '50%',
+              right: '50%'
+            }}
+          >
+            Calendar!
+          </Button>
+          {auth ? (
+            <>
+              <IconButton
+                size='large'
+                aria-controls='menu-account'
+                aria-haspopup='true'
+                onClick={e => setAnchorEl(e.currentTarget)}
+                color='inherit'
+                sx={{
+                  ml: 'auto',
+                  mr: 2
+                }}>
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id='menu-account'
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right'
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={Boolean(anchorEl)}
+                onClose={() => setAnchorEl(null)}
+              >
+                <MenuItem onClick={handleClick}>My Account</MenuItem>
+                <MenuItem onClick={handleClick}>Settings</MenuItem>
+              </Menu>
+            </>
+          ) :
+            <Box sx={{
+              ml: 'auto',
+
+            }}>
+              <Button href='login'
+                variant='outlined'
+                color='inherit'
+                sx={{ mr: 2 }}
+              >
+                Login
+              </Button>
+              <Button href='signup'
+                variant='outlined'
+                color='inherit'
+              >
+                Sign Up
+              </Button>
+            </Box>
+
+          }
         </Toolbar>
       </AppBar>
-    </div>
-  );
+    </Box>
+  )
 }
