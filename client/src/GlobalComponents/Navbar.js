8df7a0f1e0cb3f7cfddc0bf6d131d7bbf51b2e82
leowrites <75044178+leowrites@users.noreply.github.com>
2022-06-02 16:36:20 -0700

    Navbar style and function update

--- client/src/GlobalComponents/Navbar.js
+++ client/src/GlobalComponents/Navbar.js
@@ -1,18 +1,28 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Button from '@mui/material/Button'
 import Switch from '@mui/material/Switch'
 import Link from '@mui/material/Link';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem'
+import IconButton from '@mui/material/IconButton'
+import Menu from '@mui/material/Menu'
+import Divider from '@mui/material/Divider'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import Profile from './Profile'
 
 export default function Navbar() {
   const [auth, setAuth] = useState(false)
   const [friends, setFriends] = useState(false)
+  const [anchorElNav, setAnchorElNav] = useState(null)
+  const matchMd = useMediaQuery((theme) => theme.breakpoints.up('md'))
   // fake friend data
-  const [friendData, _] = useState([...Array(5).keys()])
+  const [friendData, _] = useState([...Array(3).keys()])
   // if logged in, request for friend info
+  const navigate = useNavigate()
   useEffect(() => {
     //fetch
     console.log('fetching')
@@ -43,38 +53,85 @@ export default function Navbar() {
     }
   }
 
+  const handleLogout = () => {
+    setAuth(false)
+    setAnchorElNav(null)
+    console.log('Handle Logout')
+  }
+
+  const handleMenuClick = (link) => {
+    setAnchorElNav(null)
+    link && navigate(link)
+  }
+
   const Profiles = () => {
     if (auth && friends) {
-      // pass real info into profile
-      const profiles = friendData.map((v, i) => (
-        i === 0 ? <Profile key={i} sx={{ ml: 'auto' }} handleClick={handleClick} />
-          : i === friendData.length ? <Profile key={i} sx={{ mr: 2 }} handleClick={handleClick} />
-            : <Profile key={i} handleClick={handleClick} />
+      // if user is authenticated and has friends 
+      const friendProfiles = friendData.map((v, i) => (
+        i === 0 ? <Profile key={i} sx={{ m: 1, ml: 'auto' }} handleClick={handleClick} />
+          : <Profile key={i} sx={{ m: 1 }} profileInfo handleClick={handleClick} />
+
       ))
+      // if size is larger than medium, show friends
       return (
         <>
-          {profiles}
+          {matchMd && friendProfiles}
+          {matchMd &&
+            <Divider sx={{ m: 1, display: { xs: 'none', md: 'flex' } }}
+              orientation='vertical' flexItem />}
+          <Profile isUser sx={{ ml: matchMd ? 0 : 'auto' }} profileInfo handleClick={handleClick} />
         </>
       )
     }
     else if (auth) {
+      // if user is authenticated
       return (
-        <Profile sx={{ ml: 'auto', mr: 2 }} handleClick={handleClick} />
+        <Profile isUser sx={{ ml: 'auto' }} handleClick={handleClick} />
       )
     } else {
-      return (
-        <Box sx={{ ml: 'auto', mr: 2 }}>
-          <Button href='login' variant='outlined' color='inherit' sx={{ mr: 2 }}>
-            Login
-          </Button>
-          <Button href='signup' variant='outlined' color='inherit'>
-            Sign Up
-          </Button>
-        </Box>
-      )
+      // if user is not authenticated and screen size is larger than medium
+      if (matchMd) {
+        return (
+          <Box sx={{ ml: 'auto' }}>
+            <Button href='login' variant='outlined' color='inherit' sx={{ mr: 2 }}>
+              Login
+            </Button>
+            <Button href='signup' variant='outlined' color='inherit'>
+              Sign Up
+            </Button>
+          </Box>
+        )
+      } else {
+        // placeholder
+        return <Box sx={{ ml: 'auto', mr: 6 }} />
+      }
     }
   }
 
+  const NavMenu = () => {
+    // NavMenu changes based on auth
+    return (
+      auth ?
+        <Menu id='menu-appbar' anchorEl={anchorElNav}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+          keepMounted
+          transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+          open={Boolean(anchorElNav)} onClose={() => setAnchorElNav(null)}>
+          <MenuItem onClick={() => handleMenuClick('/')}>Home</MenuItem>
+          <MenuItem onClick={() => handleMenuClick('calendar')}>Go to My Calendar</MenuItem>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
+        :
+        <Menu id='menu-appbar' anchorEl={anchorElNav}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+          keepMounted
+          transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+          open={Boolean(anchorElNav)} onClose={() => setAnchorElNav(null)}>
+          <MenuItem onClick={() => handleMenuClick('login')}>Login</MenuItem>
+          <MenuItem onClick={() => handleMenuClick('signup')}>Sign Up</MenuItem>
+        </Menu>
+    )
+  }
   return (
     <Box sx={{ flexGrow: 1 }}>
       Tool Bar
@@ -84,14 +141,32 @@ export default function Navbar() {
       {friends ? 'Hide friends' : 'Show friends'}
       <AppBar position='static'>
         <Toolbar>
-          <Link href='/' variant='h5' color='inherit' underline='none' sx={{ mx: 2 }}>
-            MyCalendar
-          </Link>
-          <Button href='/calendar' variant='text' color='inherit'
-            sx={{ fontSize: 16, position: 'absolute', left: '50%', right: '50%' }}>
-            Calendar!
-          </Button>
-          <Profiles />
+          {
+            // change order of items in appbar based on screen size
+            matchMd ?
+              <>
+                <Link href='/' variant='h5' color='inherit' underline='none' sx={{ mx: 2 }}>
+                  MyCalendar
+                </Link>
+                <Button href='/calendar' variant='text' color='inherit'
+                  sx={{ fontSize: 16, position: 'absolute', left: '50vh', right: '50vh' }}>
+                  Calendar!
+                </Button>
+                <Profiles />
+              </> :
+              <>
+                <IconButton size='large' aria-label='nav-menu' aria-controls='menu-appbar' aria-haspopup='true'
+                  onClick={e => setAnchorElNav(e.currentTarget)} color='inherit'>
+                  <MenuIcon />
+                </IconButton>
+                <NavMenu />
+                <Link href='/' variant='h5' color='inherit'
+                  underline='none' sx={{ ml: 'auto' }}>
+                  MyCalendar
+                </Link>
+                <Profiles />
+              </>
+          }
         </Toolbar>
       </AppBar>
     </Box>
--- client/src/GlobalComponents/Profile.js
+++ client/src/GlobalComponents/Profile.js
@@ -1,28 +1,58 @@
 import { useState } from 'react'
-import { IconButton } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import MenuItem from '@mui/material/MenuItem'
 import Menu from '@mui/material/Menu'
+import Popover from '@mui/material/Popover'
+import Typography from '@mui/material/Typography';
 
-export default function Profile({ sx, handleClick }) {
+export default function Profile({ sx, profileInfo, isUser, handleClick }) {
     const [anchorEl, setAnchorEl] = useState(null)
+    const open = Boolean(anchorEl)
+    // using md as breakpoints for mobile version
     return (
         <>
-            <IconButton size='large' aria-controls='menu-account' aria-haspopup='true'
-                onClick={e => setAnchorEl(e.currentTarget)} color='inherit'
-                sx={sx}>
-                <AccountCircle />
-            </IconButton>
-            <Menu id='menu-account' anchorEl={anchorEl}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                keepMounted
-                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-                open={Boolean(anchorEl)}
-                onClose={() => setAnchorEl(null)}
-            >
-                <MenuItem onClick={handleClick}>My Account</MenuItem>
-                <MenuItem onClick={handleClick}>Settings</MenuItem>
-            </Menu>
+            {
+                isUser ?
+                    <>
+                        {/* User Icon, show at all times */}
+                        <IconButton size='large' aria-controls='menu-account' aria-haspopup='true'
+                            onClick={e => setAnchorEl(e.currentTarget)} color='inherit'
+                            sx={sx}>
+                            <AccountCircle />
+                        </IconButton>
+                        <Menu id='menu-account' anchorEl={anchorEl}
+                            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                            keepMounted
+                            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                            open={Boolean(anchorEl)}
+                            onClose={() => setAnchorEl(null)}
+                        >
+                            <MenuItem onClick={handleClick}>My Account</MenuItem>
+                            <MenuItem onClick={handleClick}>Settings</MenuItem>
+                        </Menu>
+                    </>
+                    :
+                    <>
+                        {/*Friend Icon. Show on screen larger than small*/}
+                        <AccountCircle
+                            aria-controls={'mouse-over-popover'}
+                            aria-haspopup='true'
+                            onMouseEnter={e => setAnchorEl(e.currentTarget)}
+                            onMouseLeave={() => setAnchorEl(null)}
+                            color='inherit'
+                            sx={sx} />
+                        <Popover id='mouse-over-popover' sx={{ pointerEvents: 'none' }}
+                            open={open} anchorEl={anchorEl}
+                            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                            transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+                            onClose={() => setAnchorEl(null)}
+                            disableRestoreFocus
+                        >
+                            <Typography> friend </Typography>
+                        </Popover>
+                    </>
+            }
         </>
     )
 }
