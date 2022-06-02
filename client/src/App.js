ad34ce594f77d323c78e7dd6c48380e0a6773471
leowrites <75044178+leowrites@users.noreply.github.com>
2022-06-01 19:57:32 -0700

    Moved routes to index, added outlet

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
 import './App.css';
-import {Route, Routes, Link} from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-import SignUp from './chloe/pages/SignUp.js';
-import Login from './chloe/pages/Login.js';
-import Landing from './chloe/pages/Landing.js';
-import Calendar from './chloe/pages/Calendar.js';
-import ForgotPassword from './chloe/pages/ForgotPassword';
+// we will only have the layout here
+// everything else will be rendered through outlet
+
+// import SignUp from './chloe/pages/SignUp.js';
+// import Login from './chloe/pages/Login.js';
+// import Landing from './chloe/pages/Landing.js';
+// import Calendar from './chloe/pages/Calendar.js';
+// import ForgotPassword from './chloe/pages/ForgotPassword';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Landing/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/forgotpassword" element={<ForgotPassword/>} />
-          <Route path="/calendar" element={<Calendar/>}/>
-        </Routes>
-      </Router>
+    <div className='App'>
+      <Outlet />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import PageLayout from './GlobalComponents/PageLayout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './Home/SignUp';
+import Login from './Home/Login';
+import Landing from './Landing/Landing';
+import Calendar from './Calendar/Calendar';
+import ForgotPassword from './Home/ForgotPassword';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route element={<PageLayout />}>
+          <Route index element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/calendar" element={<Calendar />} />
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter >
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
