0fccc58d050b9d92de5e147e49e6b629b8291a26
leowrites <75044178+leowrites@users.noreply.github.com>
2022-06-06 18:10:08 -0700

    app.js change

--- app.js
+++ app.js
@@ -57,6 +57,8 @@ if (app.get('env') === 'production') {
  
 app.use(session(sessionOptions))
 
+app.locals.returnUrl = ''
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -68,7 +70,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
-app.use('/users/:id/events', eventsRouter)
+app.use('/events', eventsRouter)
+app.use('/courses', courseRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -82,8 +85,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // redirect toerror page in front
+  res.status(err.status || 500).redirect('/error')
 });
 
 app.listen(port, () => {
