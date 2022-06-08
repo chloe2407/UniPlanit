8020e7b24241dbdc5f9a9fff22d8cf4c27812284
leowrites <75044178+leowrites@users.noreply.github.com>
2022-06-07 19:34:09 -0700

    added endpoint for course searching

--- models/course.js
+++ models/course.js
@@ -5,23 +5,84 @@ const courseSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    sections: [{
-        sectionCode: String,
-        term: {
-            type: String,
-            uppercase: true,
-            required: true
-        },
-        prof: String,
-        // custom event here
-        time: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Event',
-            required: true
-        }]
-    }]
+    courseTitle: String,
+    // requried for identifying courses between multiple unis
+    university: {
+        type: String,
+        lowercase: true,
+        enum: ['uoft'],
+        required: true
+    },
+    term: {
+        type: String,
+        enum: ['F', 'S', 'Y'],
+        required: true
+    },
+    sections: [
+        {
+            sectionCode: {
+                type: String,
+                required: true
+            },
+            term: {
+                type: String,
+                uppercase: true,
+                enum: ['F', 'S', 'Y'],
+                required: true
+            },
+            instructors: [],
+            // will only have enough information to
+            // construct an event
+            meetingTimes: [{
+                day: {
+                    type: String,
+                    enum: ['Monday', 'Tuesday', 'Wednesday',
+                        'Thursday', 'Friday', 'Saturday',
+                        'Sunday', 'error']
+                },
+                startTime: {
+                    type: String,
+                    required: true
+                },
+                endTime: {
+                    type: String,
+                    required: true
+                },
+                assignedRoom1: String
+            }]
+        }
+    ],
+    tutorials: [
+        {
+            tutorialCode: String,
+            term: {
+                type: String,
+                uppercase: true,
+                enum: ['F', 'S', 'Y'],
+                required: true
+            },
+            meetingTimes: [{
+                day: {
+                    type: String,
+                    enum: ['Monday', 'Tuesday', 'Wednesday',
+                        'Thursday', 'Friday', 'Saturday',
+                        'Sunday', 'error']
+                },
+                startTime: {
+                    type: String,
+                    required: true
+                },
+                endTime: {
+                    type: String,
+                    required: true
+                },
+                assignedRoom1: String
+            }]
+        }
+
+    ]
 })
 
 const Course = mongoose.model('Course', courseSchema)
 
-export default Course
+module.exports = Course
