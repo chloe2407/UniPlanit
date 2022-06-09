4717e2177fb4c8ea3071be93af2fdff07e49fba8
leowrites <75044178+leowrites@users.noreply.github.com>
2022-06-08 19:57:19 -0700

    included dayjs

--- utils/time.js
+++ utils/time.js
@@ -1,11 +1,27 @@
-module.exports.calibrateTimeToUTC = (date, option) => {
-    return new Date(date.getTime() + date.getTimezoneOffset() * 60000)
-}
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
-module.exports.getDate = (date, day, hour) => {
-    // to be implemented
-}
+module.exports.toUTC = date => date.utc().format()
 
-module.exports.dayToMs = (day) => {
-    return day * 60000
+module.exports.toCurrent = date => date.tz(dayjs.tz.guess()).format()
+
+module.exports.getEventDateTime = (date, day, hour) => {
+    // day = monday hour = 11:00
+    const weekStart = date.startOf('week')
+    // map day of week to num
+    const stringToInt = {
+        'Sunday': 0,
+        'Monday': 1,
+        'Tuesday': 2,
+        'Wednesday': 3,
+        'Thursday': 4,
+        'Friday': 5,
+        'Saturday': 6
+    }
+    const hourNum = parseInt(hour.substring(0, 2))
+    const startTime = weekStart.set('day', stringToInt[day]).set('hour', hourNum)
+    return startTime
 }
