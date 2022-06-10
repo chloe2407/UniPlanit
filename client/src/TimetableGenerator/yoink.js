e5cbf4870d2d5ae120a53ca642c1921489b59f29
Sarah <lemiex@outlook.com>
2022-06-10 01:09:03 -0400

    deleted yoink.js and courses_copy

--- client/src/TimetableGenerator/yoink.js
+++
@@ -1,7 +0,0 @@
-var data = require('./courses.json'); //(with path)
-// for (let course in data) {
-//     var dataList = Object.values(data);
-//     console.log(dataList);
-// }
-
-console.log(data['SOC499Y1']);
