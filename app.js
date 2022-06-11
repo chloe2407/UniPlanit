e6cf54586e9e4f82ffb8e1e22fa949daa2f00cda
leowrites <75044178+leowrites@users.noreply.github.com>
2022-06-10 22:44:17 -0700

    cleaned app

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const cors = require('cors');
-const bodyParser = require('body-parser')
 
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-const passportLocalMongoose = require('passport-local-mongoose')
 
 const swaggerJSDoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
@@ -32,7 +30,7 @@ mongoose.connect(process.env.MONGO_URL)
   .catch(err => console.error(err))
 
 mongoose.connection.on('error', err => {
-    console.error(err)
+  console.error(err)
 })
 
 mongoose.connection.on('disconnected', err => {
@@ -53,6 +51,7 @@ const sessionOptions = {
   cookie: {
     httpOnly: true,
     // set to true in production
+    maxAge: 24 * 60 * 60 * 1000,
     secure: false
   },
   store: MongoStore.create(mongoStoreOptions),
@@ -71,25 +70,23 @@ const swaggerDefinition = {
 
 const swaggerSpec = swaggerJSDoc({
   swaggerDefinition,
-  apis: [`${__dirname}/routes/*.js`, 
-        `${__dirname}/routes/parameters.yaml`]
+  apis: [`${__dirname}/routes/*.js`,
+  `${__dirname}/routes/parameters.yaml`]
 })
 
 if (app.get('env') === 'production') {
   app.set('trsut proxy', 1)
   sessionOptions.cookie.secure = true
 }
- 
-app.use(session(sessionOptions))
 
-app.locals.returnUrl = ''
+app.use(session(sessionOptions))
+app.use(passport.initialize());
+app.use(passport.session());
 
 //passport stuff
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());       //session encoding
 passport.deserializeUser(User.deserializeUser());   //session decoding
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 // view engine setup
@@ -97,13 +94,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.get('/', (req, res) => {
-  res.render('index.jade', {title:'work goddamit'})
+  res.render('index.jade', { title: 'work goddamit' })
 })
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -113,22 +110,22 @@ app.use('/courses', courseRouter)
 app.use(cors())
 // app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({extended : true}));
- 
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(new ExpressError('Page not found', 404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error.jade', {title:'your_page_title'});
+  res.render('error.jade', { title: 'your_page_title' });
   // redirect toerror page in front
   console.error(err.message)
   res.status(err.status || 500)
