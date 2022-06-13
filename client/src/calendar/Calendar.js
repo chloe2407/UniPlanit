40d77c793e77b4ac8361014e4e213033f38ac483
Chloe Lam <chloelam2407@gmail.com>
2022-06-12 18:10:13 -0700

    basic calendar

--- client/src/calendar/Calendar.js
+++ client/src/calendar/Calendar.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import { React, useState} from 'react';
 import { Grid } from '@material-ui/core';
 
-import WeekView from './weekview';
+import WeekView from './weekview.js';
+import OptionsTab from './optionstab.js'
 
 
 const Calendar = () => {
+    const [currentSession, setCurrentSession] = useState(0);
     return (
         <div>
             <Grid container>
                 <Grid item xs={1} sm={3} style={{backgroundColor: 'yellow'}}>side menu</Grid>
-                <Grid item xs={12} sm={9}>
-                    <WeekView/>
+                <Grid item xs={11} sm={9}>
+                    <OptionsTab setCurrentSession={setCurrentSession}/>
+                    <WeekView currentSession={currentSession}/>
                 </Grid>
             </Grid>
         </div>
