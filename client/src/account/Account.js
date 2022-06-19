246e8f62c5872825f300a0e0c6529d2d6eeb8a91
Sarah <lemiex@outlook.com>
2022-06-19 13:07:59 -0400

    Added schedule cards

--- client/src/account/Account.js
+++ client/src/account/Account.js
@@ -14,22 +14,19 @@ const Account = () => {
   const [imgUrl, loadImg] = useImg()
   const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
-    document.body.style.overflow = 'hidden'
     loadImg()
       .then(() => {
         setIsLoading(false)
       })
-    // eslint-disable-next-line
   }, []
   )
-
 
   return (
     <>
       <div style={{
-        backgroundImage: imgUrl && `url(${imgUrl})`, display: 'flex', height: '25vh',
+        backgroundImage: imgUrl && `url(${imgUrl})`, display: 'flex', height: '17rem',
+        backgroundPosition: 'center',
         backgroundSize: 'cover',
-        justifyContent: 'center',
         justifyContent: 'center',
         alignItems: 'center',
       }}>
@@ -43,11 +40,12 @@ const Account = () => {
           alignItems="flex-start"
           paddingTop='70px'
         >
-          <Grid item xs={12} md={5} xl={6}>
+          <Grid item xs={12} md={4} xl={5}>
             <ProfileImage />
 
           </Grid>
-          <Grid item padding='30px' xs={12} md={7} xl={6}>
+          <Grid item paddingLeft='40px'
+            paddingRight='40px' xs={12} md={7} xl={6}>
             <AccountInfo />
           </Grid>
         </Grid>
--- client/src/account/components/Favorites/ScheduleCard.js
+++ client/src/account/components/Favorites/ScheduleCard.js
@@ -1,31 +1,53 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
-const ScheduleCard = ({ name }) => {
+const ScheduleCard = ({ name, filters, snapshot }) => {
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image='./public/ex_snapshot.png'
-                    alt="green iguana"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
-                        {name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Filters
+        <Grid item xs={12} sm={6} md={4} xl={3} display='flex' justifyContent='center' >
+            <Card sx={{
+                width: 270, marginBottom: '2rem'
+            }} >
+                <CardActionArea>
+                    < CardMedia
+                        component="img"
+                        height="140"
+                        image={snapshot}
+                        alt={name}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h6" component="div">
+                            {name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            <Stack direction="row" spacing={1}>
+                                {
+                                    filters.length < 3
+                                        ?
+                                        filters.map((filter) => (
+                                            <Chip label={filter} />
+                                        ))
+                                        :
+                                        <>
+                                            <Chip label={filters[0]} />
+                                            <Chip label={filters[1]} />
+                                            <Chip label={'+' + (filters.length - 2)} />
+                                        </>
 
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                                }
+                            </Stack>
+
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card >
+        </Grid >
     )
 }
 
