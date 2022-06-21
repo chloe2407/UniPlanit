14f6aba0950a9cf3881882a04856e889eb94e7b6
ileuxali <leowendali@gmail.com>
2022-06-20 20:14:36 -0400

    made sidebar intuitive to close (madge to change)

--- client/src/calendar/Calendar.js
+++ client/src/calendar/Calendar.js
@@ -7,49 +7,63 @@ import SideMenu from './sidemenu';
 import Collapse from '@mui/material/Collapse';
 import Drawer from '@mui/material/Drawer';
 import Grid from '@mui/material/Grid';
-import Divider from '@mui/material/Divider'
-
+import Divider from '@mui/material/Divider';
 
 const Calendar = () => {
-    const [currentSession, setCurrentSession] = useState(0);
-    const [openDrawer, setOpenDrawer] = useState(false)
-    const [openEdit, setOpenEdit] = useState(false)
-    const handleOpenDrawer = () => {
-        setOpenDrawer(true)
-    }
-    const handleCloseDrawer = () => {
-        setOpenDrawer(false)
-    }
-    return (
-        // <Grid container sx={{ height: '100vh'}}>
-        //     <Grid item xs={3} sm={3} sx={{ p: 2 }}>
-        <>
-            <Drawer open={openDrawer} anchor='left' variant='persistent'>
-                {/* <SideMenu /> */}
-                {openEdit ? 
-                <Grid container sx={{ height: '100vh', width: '100vw'}}>
-                    <Grid item xs={3} sm={3} sx={{ p: 2, borderRight: 1}}>
-                        <SideMenu handleCloseDrawer={handleCloseDrawer} openEdit={openEdit} setOpenEdit={setOpenEdit}/>
-                    </Grid>
-                    {/* <Divider sx={{ mt: 1, mb: 1, mx: 2 }} orientation='vertical'/> */}
-                    <Grid item xs={9} sm={9}>
-                        <Collapse in={openEdit} unmountOnExit>
-                            <OptionsTab setCurrentSession={setCurrentSession} />
-                            <WeekView currentSession={currentSession} />
-                        </Collapse>
-                    </Grid>
-                </Grid> :
-                <Grid container sx={{ height: '100vh', width: '25vw'}}>
-                    <Grid item xs={12} sm={12} sx={{ p: 2, borderRight: 1}}>
-                        <SideMenu handleCloseDrawer={handleCloseDrawer} openEdit={openEdit} setOpenEdit={setOpenEdit}/>
-                    </Grid>
-                </Grid>}
-            </Drawer>
-            <div sx={{zIndex:1}}>
-            <OptionsTab openDrawer={handleOpenDrawer} setCurrentSession={setCurrentSession} />
-            <WeekView currentSession={currentSession} />
-            </div>
-            {/* <div sx={{zIndex: 2 }}>
+  const [currentSession, setCurrentSession] = useState(0);
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
+  const handleOpenDrawer = () => {
+    setOpenDrawer(true);
+  };
+  const handleCloseDrawer = () => {
+    setOpenDrawer(false);
+  };
+  return (
+    // <Grid container sx={{ height: '100vh'}}>
+    //     <Grid item xs={3} sm={3} sx={{ p: 2 }}>
+    <>
+      <Drawer
+        open={openDrawer}
+        anchor="left"
+        variant="temporary"
+        onClose={() => setOpenDrawer(false)}
+      >
+        {/* <SideMenu /> */}
+        {openEdit ? (
+          <Grid container sx={{ height: '100vh', width: '100vw' }}>
+            <Grid item xs={3} sm={3} sx={{ p: 2, borderRight: 1 }}>
+              <SideMenu
+                handleCloseDrawer={handleCloseDrawer}
+                openEdit={openEdit}
+                setOpenEdit={setOpenEdit}
+              />
+            </Grid>
+            {/* <Divider sx={{ mt: 1, mb: 1, mx: 2 }} orientation='vertical'/> */}
+            <Grid item xs={9} sm={9}>
+              <Collapse in={openEdit} unmountOnExit>
+                <OptionsTab setCurrentSession={setCurrentSession} />
+                <WeekView currentSession={currentSession} />
+              </Collapse>
+            </Grid>
+          </Grid>
+        ) : (
+          <Grid container sx={{ height: '100vh', width: '25vw' }}>
+            <Grid item xs={12} sm={12} sx={{ p: 2, borderRight: 1 }}>
+              <SideMenu
+                handleCloseDrawer={handleCloseDrawer}
+                openEdit={openEdit}
+                setOpenEdit={setOpenEdit}
+              />
+            </Grid>
+          </Grid>
+        )}
+      </Drawer>
+      <div sx={{ zIndex: 1 }}>
+        <OptionsTab openDrawer={handleOpenDrawer} setCurrentSession={setCurrentSession} />
+        <WeekView currentSession={currentSession} />
+      </div>
+      {/* <div sx={{zIndex: 2 }}>
 
             <Grid container sx={{ height: '100vh'}}>
             <Grid item xs={3} sm={3} sx={{ p: 2 }}>
@@ -61,14 +75,14 @@ const Calendar = () => {
             </Grid>
             </Grid>
             </div> */}
-        </>
-        //     </Grid>
-        // </Grid>
-        //     <Collapse in={open} timeout="auto">
-        //             <SideMenu false={openEdit} setOpenEdit={() => setOpenEdit()}/>
-        //     </Collapse>E
-        // </Box>
-    )
-}
+    </>
+    //     </Grid>
+    // </Grid>
+    //     <Collapse in={open} timeout="auto">
+    //             <SideMenu false={openEdit} setOpenEdit={() => setOpenEdit()}/>
+    //     </Collapse>E
+    // </Box>
+  );
+};
 
 export default Calendar;
