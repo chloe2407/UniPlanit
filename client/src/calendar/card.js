1c35bace8a1894e7ef413c53e63d2fac53a93050
leowrites <75044178+leowrites@users.noreply.github.com>
2022-06-22 15:49:16 -0400

    Calendar Change

--- client/src/calendar/card.js
+++ client/src/calendar/card.js
@@ -12,97 +12,119 @@ import LockOpenIcon from '@mui/icons-material/LockOpen';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
-import schedule from "./schedule.js";
+import schedule from './schedule.js';
 
-const useStyles = makeStyles(theme => ({
-    container: {
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        alignItems: "stretch",
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    alignItems: 'stretch',
+  },
+  text: {
+    [theme.breakpoints.down('md')]: {
+      fontSize: '1.1vw',
     },
-    text: {
-         [theme.breakpoints.down('md')]: {
-            fontSize: "1.1vw",
-         },
-         [theme.breakpoints.down('sm')]: {
-            fontSize: "1.3vw",
-         },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '1.3vw',
     },
-    buttons: {
-        width: "25%",
-        [theme.breakpoints.down('xs')]: {
-            width: "10%",
-         },
-        
-    }
-}))
+  },
+  buttons: {
+    width: '25%',
+    [theme.breakpoints.down('xs')]: {
+      width: '10%',
+    },
+  },
+}));
 
-const handleChange = () => {
-    
+const handleChange = () => {};
 
-}
+const EventCard = (course, currentSchedule) => {
+  const classes = useStyles();
+  var selectedSchedule = currentSchedule;
+  var selectedCourse = course;
 
+  var smallClass;
+  if (
+    Number(selectedCourse.endTime.slice(0, 2)) -
+      Number(selectedCourse.startTime.slice(0, 2)) <
+    2
+  ) {
+    smallClass = true;
+  }
 
-const EventCard = (course, currentSchedule)  => {
-    const classes = useStyles();
-    var selectedSchedule = currentSchedule
-    var selectedCourse = course
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-    var smallClass
-    if (Number(selectedCourse.endTime.slice(0,2)) - Number(selectedCourse.startTime.slice(0,2)) < 2) {
-        smallClass = true
-    }
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-  
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
-    return (
-        <Box className={classes.container} style={{flexDirection: smallClass? "row": "column"}}>
-            <div style={{display: "flex", justifyContent: "flexStart", flexDirection: "column", marginLeft: "1vw"}}>
-                    <Typography variant='body' style={{display: "flex"}} className={classes.text}>
-                        {selectedCourse.courseName} {selectedCourse.courseSection} {selectedCourse.courseSession}
-                    </Typography>
-                    <Typography variant='body' style={{display: smallClass? "none": "flex"}} className={classes.text}>{selectedCourse.startTime}-{selectedCourse.endTime} || {selectedCourse.location}</Typography>
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+  return (
+    <Box
+      className={classes.container}
+      style={{ flexDirection: smallClass ? 'row' : 'column' }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flexStart',
+          flexDirection: 'column',
+          marginLeft: '1vw',
+        }}
+      >
+        <Typography
+          variant="body"
+          style={{ display: 'flex' }}
+          className={classes.text}
+        >
+          {selectedCourse.courseName} {selectedCourse.courseSection}{' '}
+          {selectedCourse.courseSession}
+        </Typography>
+        <Typography
+          variant="body"
+          style={{ display: smallClass ? 'none' : 'flex' }}
+          className={classes.text}
+        >
+          {selectedCourse.startTime}-{selectedCourse.endTime} ||{' '}
+          {selectedCourse.location}
+        </Typography>
+      </div>
+      <Stack direction="row">
+        <IconButton className={classes.buttons}>
+          <LockIcon fontSize="small" />
+        </IconButton>
+        <IconButton className={classes.buttons}>
+          <DeleteIcon fontSize="small" />
+        </IconButton>
+        <IconButton className={classes.buttons}>
+          <MoreHorizIcon fontSize="small" onClick={handleClick} />
+        </IconButton>
+      </Stack>
 
-                </div>
-                <Stack direction="row">
-                    <IconButton className={classes.buttons}><LockIcon fontSize='small'/></IconButton>
-                    <IconButton className={classes.buttons}><DeleteIcon fontSize='small' /></IconButton>
-                    <IconButton className={classes.buttons}><MoreHorizIcon fontSize='small' onClick={handleClick} /></IconButton>
-
-                </Stack>
-                
-                <Popover 
-                 id={id}
-                 open={open}
-                 anchorEl={anchorEl}
-                 onClose={handleClose}
-                 anchorOrigin={{
-                    vertical: 'center',
-                    horizontal: 'center',
-                 }}
-                 transformOrigin={{
-                    vertical: 'center',
-                    horizontal: 'center',
-                 }}
-                >
-                The content of the Popover.
-                </Popover>
-
-        </Box>
-        
-    
-    );
-}
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'center',
+        }}
+      >
+        The content of the Popover.
+      </Popover>
+    </Box>
+  );
+};
 
 export default EventCard;
