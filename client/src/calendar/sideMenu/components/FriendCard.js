46d0dfe29ed8d3b1c5aee65272c5f6868d4f4390
leowrites <75044178+leowrites@users.noreply.github.com>
2022-06-29 21:57:10 -0400

    Added support for viewing friend's timetables

---
+++ client/src/calendar/sideMenu/components/FriendCard.js
@@ -0,0 +1,42 @@
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import theme from 'theme/theme';
+import Stack from '@mui/material/Stack';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import useCalendar from 'context/calendar';
+
+export default function FriendCard({ friendInfo }) {
+  // collapse show friend timetables
+  const { setView, setCurrentFriend } = useCalendar();
+  return (
+    <Card
+      sx={{
+        px: 2,
+        color: 'white',
+        backgroundColor: theme.palette.primary.main,
+      }}
+    >
+      <Stack
+        direction={'row'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+      >
+        <Typography> {`${friendInfo.first} ${friendInfo.last}`}</Typography>
+        <CardActions>
+          <IconButton
+            sx={{ px: 0, mx: 0, color: theme.palette.secondary.main }}
+            onClick={() => {
+              setView('friend fav');
+              setCurrentFriend(friendInfo);
+            }}
+          >
+            <ArrowForwardIosIcon />
+          </IconButton>
+        </CardActions>
+      </Stack>
+    </Card>
+  );
+}
--- client/src/calendar/sideMenu/components/TimetableCard.js
+++ client/src/calendar/sideMenu/components/TimetableCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
@@ -9,61 +9,126 @@ import CardContent from '@mui/material/CardContent';
 import CardActionArea from '@mui/material/CardActionArea';
 import Stack from '@mui/material/Stack';
 import CardActions from '@mui/material/CardActions';
+import theme from 'theme/theme';
+import useCalendar from 'context/calendar';
+import CompareIcon from '@mui/icons-material/Compare';
+import { StyledPopover } from 'globalComponents/navbar/NavbarMenu';
 
 export default function TimetableCard({
   timetableIndex,
-  timetable,
-  handleAddFavourite,
-  favTimetable,
-  getMatchTimetable,
+  tb,
+  handleAddFavourite = null,
+  favTimetable = null,
+  getMatchTimetable = null,
   setTimetableIndex,
   index,
+  isAuthor = true,
 }) {
+  const { setCurrentTimetableCompare, userFriend } = useCalendar();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
   return (
-    <Card
+    <Box
       sx={{
-        my: 2,
-        border:
-          timetableIndex === index ? '3px solid #576066' : '3px solid white',
         transition: (theme) =>
-          theme.transitions.create('border', {
+          theme.transitions.create(['border', 'transform'], {
             easing: theme.transitions.easing.easeInOut,
             duration: 200,
           }),
+        ':hover': {
+          transform: 'scale(1.05)',
+        },
       }}
-      key={index}
     >
-      <CardActionArea
-        onClick={() => {
-          setTimetableIndex(index);
+      <Card
+        sx={{
+          pb: !favTimetable && 2,
+          my: 2,
+          color: 'white',
+          backgroundColor: theme.palette.primary.main,
+          // border:
+          //   timetableIndex === index ? '3px solid #576066' : '3px solid black',
+          transform: timetableIndex === index ? 'scale(1.05)' : 'scale(0.95)',
+          transition: (theme) =>
+            theme.transitions.create(['border', 'transform'], {
+              easing: theme.transitions.easing.easeInOut,
+              duration: 200,
+            }),
         }}
+        key={index}
       >
-        <CardContent sx={{ pt: 1, pb: 0 }}>
-          <Box>
-            {timetable.map((course) => (
-              <Box key={course.courseCode} sx={{ textAlign: 'start' }}>
-                <Stack direction={'row'}>
-                  <Typography sx={{}}>{course.courseCode}</Typography>
-                  <Typography sx={{ ml: 'auto' }}>
-                    {course.section.sectionCode}
-                  </Typography>
-                  <Typography>{course.tutorial}</Typography>
-                </Stack>
-              </Box>
-            ))}
-          </Box>
-        </CardContent>
-      </CardActionArea>
+        <CardActionArea
+          onClick={() => {
+            setTimetableIndex(index);
+          }}
+        >
+          <CardContent sx={{ pt: 1, pb: 0 }}>
+            <Box>
+              {tb.timetable.map((course) => (
+                <Box key={course.courseCode} sx={{ textAlign: 'start' }}>
+                  <Stack direction={'row'}>
+                    <Typography sx={{}}>{course.courseCode}</Typography>
+                    <Typography sx={{ ml: 'auto' }}>
+                      {course.section.sectionCode}
+                    </Typography>
+                    <Typography>{course.tutorial}</Typography>
+                  </Stack>
+                </Box>
+              ))}
+            </Box>
+          </CardContent>
+        </CardActionArea>
 
-      <CardActions sx={{ my: 0, py: 0 }}>
-        <IconButton onClick={() => handleAddFavourite(timetable)}>
-          {favTimetable && getMatchTimetable(timetable) ? (
-            <FavoriteIcon />
-          ) : (
-            <FavoriteBorderIcon />
-          )}
-        </IconButton>
-      </CardActions>
-    </Card>
+        {isAuthor && (
+          <CardActions sx={{ my: 0, py: 0 }}>
+            <IconButton onClick={() => handleAddFavourite(tb)}>
+              {favTimetable && getMatchTimetable(tb.timetable) ? (
+                <FavoriteIcon sx={{ color: 'white' }} />
+              ) : (
+                <FavoriteBorderIcon sx={{ color: 'white' }} />
+              )}
+            </IconButton>
+            <IconButton
+              onClick={(e) => {
+                setAnchorEl(e.currentTarget);
+                setCurrentTimetableCompare(tb);
+              }}
+            >
+              <CompareIcon sx={{ color: 'white' }} />
+            </IconButton>
+            <StyledPopover
+              sx={{
+                '& .MuiPopover-paper': {
+                  backgroundColor: 'gray',
+                },
+              }}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={() => setAnchorEl(null)}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+            >
+              <Box sx={{ p: 2 }}>
+                <strong>
+                  <Typography>Compare With a FrNiend</Typography>
+                </strong>
+                {userFriend ? (
+                  userFriend.map((friend) => (
+                    <Box key={friend._id} sx={{ my: 1 }}>
+                      <Typography>{`${friend.first} ${friend.last}`}</Typography>
+                    </Box>
+                  ))
+                ) : (
+                  <Typography>Your friends will show up here</Typography>
+                )}
+              </Box>
+            </StyledPopover>
+          </CardActions>
+        )}
+      </Card>
+    </Box>
   );
 }
