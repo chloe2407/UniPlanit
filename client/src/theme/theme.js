679675e97a5e09e556755b670be82bcd77bded5b
leowrites <75044178+leowrites@users.noreply.github.com>
2022-07-02 01:07:57 -0400

    theme update

--- client/src/theme/theme.js
+++ client/src/theme/theme.js
@@ -1,11 +1,11 @@
 import { createTheme } from '@mui/material/styles';
-import '@fontsource/josefin-sans/100.css';
-import '@fontsource/josefin-sans/200.css';
-import '@fontsource/josefin-sans/300.css';
-import '@fontsource/josefin-sans/400.css';
-import '@fontsource/josefin-sans/500.css';
-import '@fontsource/josefin-sans/600.css';
-import '@fontsource/josefin-sans/700.css';
+// import '@fontsource/josefin-sans/100.css';
+// import '@fontsource/josefin-sans/200.css';
+// import '@fontsource/josefin-sans/300.css';
+// import '@fontsource/josefin-sans/400.css';
+// import '@fontsource/josefin-sans/500.css';
+// import '@fontsource/josefin-sans/600.css';
+// import '@fontsource/josefin-sans/700.css';
 import '@fontsource/spectral';
 
 const theme = createTheme({
